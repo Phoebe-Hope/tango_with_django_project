82812c90649503e82760b3aa2445d994d7fd0dff
Phoebe-Hope <2739337h@student.gla.ac.uk>
2025-01-22 19:54:25 +0000

    Created templates and static directory

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Rango says hey there partner!</h1><p><a href='/rango/about'>About</a></p>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("<h1>Rango says here is the about page.</h1><p><a href='/rango/'>Index</a></p>")
