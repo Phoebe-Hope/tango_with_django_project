cc70d9a21bc85dde97e4bb6301e561ea588e4c61
Phoebe-Hope <2739337h@student.gla.ac.uk>
2025-01-23 19:54:32 +0000

    Finished 6.2 and it works

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+#import category model
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    #query database for list of all categories
+    #order categories by likes, descending
+    #retrive top 5 only
+    #place list in context_dict dictionary
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
+    #render response and send it back
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
